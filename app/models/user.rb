ec1b070a343373b2b773e3096d21ef10ea0700dc
plee1186 <plee1186@gmail.com>
2015-11-12 07:10:51 +0000

    jennifer!

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User
   field :gender, type: String
   field :yob, type: Integer
   field :tos, type: Mongoid::Boolean
-  validates :tos, :inclusion => {:in => [true]}
+  #validates :tos, :inclusion => {:in => [true]}
   has_and_belongs_to_many :challenges
   
-  validates :email, presence: true
-  validates :yob, presence: true, numericality: { only_integer: true, less_than_equal_to: 2000 }
+ # validates :email, presence: true
+ # validates :yob, presence: true, numericality: { only_integer: true, less_than_equal_to: 2000 }
 end
