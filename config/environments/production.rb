a8260578d40a0b85c768c88dbe1cf3876621d718
plee1186 <plee1186@gmail.com>
2015-11-27 03:45:34 +0000

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
+  config.action_mailer.default_url_options = { :host => 'shlproject.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  
 end
