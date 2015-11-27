1598e205f596cc25fb52a769278ce958a147e173
plee1186 <plee1186@gmail.com>
2015-11-27 03:15:46 +0000

    re-routed challenges#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    #root 'challenges#index'
-  root 'users#index'
+  root 'challenges#index'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
