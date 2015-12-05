ce5fad67d5c17f6c94f96e587918e2ed66fbdf10
plee1186 <plee1186@gmail.com>
2015-12-05 06:27:13 +0000

    safe token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,18 +16,14 @@ class ApplicationController < ActionController::Base
 
   private
   
-  # For this example, we are simply using token authentication
-  # via parameters. However, anyone could use Rails's token
-  # authentication features to get the token from a header.
   def authenticate_user_from_token!
-    user_token = params[:user_token].presence
-    user       = user_token && User.find_by_authentication_token(user_token.to_s)
+    user_email = params[:user_email].presence
+    user       = user_email && User.find_by_email(user_email)
 
-    if user
-      # Notice we are passing store false, so the user is not
-      # actually stored in the session and a token is needed
-      # for every request. If you want the token to work as a
-      # sign in token, you can simply remove store: false.
+    # Notice how we use Devise.secure_compare to compare the token
+    # in the database with the token given in the params, mitigating
+    # timing attacks.
+    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
       sign_in user, store: false
     end
   end
