1e443746697063e2b92910e40483d0da889cc917
plee1186 <plee1186@gmail.com>
2015-12-06 13:11:24 +0000

    hjk

--- app/controllers/user_specific_controller.rb
+++ app/controllers/user_specific_controller.rb
@@ -16,17 +16,17 @@ class UserSpecificController < ApplicationController
     c_id = params['challenge_ids']
     c_id = c_id.to_s
     @challenge = Challenge.find(c_id)
-    #render json: JSON.pretty_generate(JSON.parse(@challenge.to_json()))
+    render json: JSON.pretty_generate(JSON.parse(@challenge.to_json()))
     
-    if @user.push(challenge_ids: @challenge.id)
-      respond_to do |format|
-        format.html { redirect_to user_specific_list_path, notice: "#{@challenge.name} completed. Congrats!" }
-      end
-    else
-      respond_to do |format|
-       format.html { redirect_to user_specific_add_path, notice: "Something went wrong. Please try again" }
-     end
-    end
+#     if @user.push(challenge_ids: @challenge.id)
+#       respond_to do |format|
+#         format.html { redirect_to user_specific_list_path, notice: "#{@challenge.name} completed. Congrats!" }
+#       end
+#     else
+#       respond_to do |format|
+#       format.html { redirect_to user_specific_add_path, notice: "Something went wrong. Please try again" }
+#      end
+#     end
   end
 end
 
