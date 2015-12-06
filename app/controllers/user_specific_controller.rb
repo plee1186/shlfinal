51358711a1a4d632b9a9797898d85f3ad0a14124
plee1186 <plee1186@gmail.com>
2015-12-06 22:46:48 +0000

    figured sessions

--- app/controllers/user_specific_controller.rb
+++ app/controllers/user_specific_controller.rb
@@ -1,6 +1,6 @@
 class UserSpecificController < ApplicationController
   
-  def list
+  def profile
     @user = current_user
     #render json: JSON.pretty_generate(JSON.parse(User.all.to_json()))
     #render json: JSON.pretty_generate(JSON.parse(Challenge.all.to_json()))
@@ -17,22 +17,7 @@ class UserSpecificController < ApplicationController
     @challenge = Challenge.find(c_id)
     render json: JSON.pretty_generate(JSON.parse(@challenge.to_json()))
     
-#     if @user.push(challenge_ids: @challenge.id)
-#       respond_to do |format|
-#         format.html { redirect_to user_specific_list_path, notice: "#{@challenge.name} completed. Congrats!" }
-#       end
-#     else
-#       respond_to do |format|
-#       format.html { redirect_to user_specific_add_path, notice: "Something went wrong. Please try again" }
-#      end
-#     end
   end
-  
-  # private
-  
-  # def authenticate_scope!
-  #   send(:"authenticate_#{resource_name}!", force: true)
-  #   self.resource = send(:"current_#{resource_name}")
-  # end
+
 end
 
